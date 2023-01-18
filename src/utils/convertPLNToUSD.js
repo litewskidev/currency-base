0c56c7d075634bf1e636ab6ec2b9feac6658492a
Adrian Litewski <litewskidev@gmail.com>
2023-01-18 02:18:29 +0100

    TEST FIX ??

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -7,7 +7,19 @@ export const convertPLNToUSD = (PLN) => {
     currency: 'USD'
   });
 
-
+  // TEST
+  if(typeof PLN === 'string'){
+    return NaN
+  };
+  if(typeof PLN === 'undefined'){
+    return NaN
+  };
+  if(typeof PLN !== 'string' && typeof PLN !== 'number'){
+    return 'Error'
+  };
+  if(PLN < 0){
+    return '$0.00'
+  };
 
   return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
 };
